9fefbb52135bf0831a7124335f46206707f9a5f7
Niklas Hofer <niklas+dev@lanpartei.de>
2014-09-14 19:50:56 +0200

    join files split by typo

--- spec/miwomi/translation_spec.rb
+++ spec/miwomi/translation_spec.rb
@@ -1,6 +1,22 @@
 require 'spec_helper'
 
 describe Miwomi::Translation do
+  context '#to_midas' do
+    let(:from) { double('FromBlock').as_null_object }
+    let(:to)   { double('ToBlock').as_null_object }
+    subject { described_class.new from, to }
+    it 'produces readable format for mIDas' do
+      from.stub id: 23
+      to.stub id: 42
+      subject.to_midas.should =~ /^23 -> 42$/
+    end
+    it 'comments using names' do
+      from.stub name: 'Iron'
+      to.stub name: 'Pudding'
+      subject.to_midas.should =~ /^# Iron => Pudding$/
+    end
+  end
+
   context '#to_yaml' do
     subject { described_class.new from, to }
     let(:from) { thing id: 23, name: "Water" }
